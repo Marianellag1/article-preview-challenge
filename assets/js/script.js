4bbee429b396203f42df85d2384123b94e10d4c6
Mgamboa <marianellag400@gmail.com>
2023-12-14 18:34:04 -0600

    got toggle to work in js for share and social icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
 // console.log("hello world"); it works
+
+var hiddenIcon = document.getElementById("hidden-icon");
+var arrowBtn = document.getElementById("btn");
+var socialIcon = document.getElementById("social-icon");
+
+arrowBtn.addEventListener("click", (e) => {
+    hiddenIcon.classList.toggle('hidden');
+    // socialIcon.classList.toggle('hidden');
+})
