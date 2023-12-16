6214e188b44f50338b71f8e0a8d4830908a7ea8d
Mgamboa <marianellag400@gmail.com>
2023-12-15 18:05:32 -0600

    re-did author layout, help from someone elses finished challenge.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,13 @@
 
 var hiddenIcon = document.getElementById("hidden-icon");
 var arrowBtn = document.getElementById("btn");
-var socialIcon = document.getElementById("social-icon");
+// var authorMichelle =document.querySelector(".author");
+// var cardShare = document.querySelector(".card-share");
+// var socialIcon = document.getElementById("social-icon");
 
 arrowBtn.addEventListener("click", (e) => {
     hiddenIcon.classList.toggle('hidden');
+    // authorMichelle.classList.toggle('hidden');
+
     // socialIcon.classList.toggle('hidden');
 })
